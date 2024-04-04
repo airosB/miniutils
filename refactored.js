603cc3ae6beb0ed640bd1e0b40d30a67aa273b37
airosB <airos.berlanathe@gmail.com>
2024-04-04 18:59:10 +0900

    blogger

--- refactored.js
+++ refactored.js
@@ -1,8 +1,5 @@
-const $ = (selector) => {
+const _ = (selector) => {
     return document.querySelector(selector);
-};
-const $$ = (selector) => {
-    return document.querySelectorAll(selector);
 };
 
 const main = () => {
@@ -11,6 +8,56 @@ const main = () => {
             return str.replace(/＄/g, '$').replace(/％/g, '%').replace(/＆/g, '&').replace(/：/g, ':').replace(/；/g, ';').replace(/－/g, '-').replace(/＾/g, '^').replace(/＠/g, '@').replace(/．/g, '.').replace(/＊/g, '*').replace(/／/g, ' / ').replace(/＿/g, '_').replace(/　/g, ' ').replace(/１/g, '1').replace(/２/g, '2').replace(/３/g, '3').replace(/４/g, '4').replace(/５/g, '5').replace(/６/g, '6').replace(/７/g, '7').replace(/８/g, '8').replace(/９/g, '9').replace(/０/g, '0').replace(/Ａ/g, 'A').replace(/Ｂ/g, 'B').replace(/Ｃ/g, 'C').replace(/Ｄ/g, 'D').replace(/Ｅ/g, 'E').replace(/Ｆ/g, 'F').replace(/Ｇ/g, 'G').replace(/Ｈ/g, 'H').replace(/Ｉ/g, 'I').replace(/Ｊ/g, 'J').replace(/Ｋ/g, 'K').replace(/Ｌ/g, 'L').replace(/Ｍ/g, 'M').replace(/Ｎ/g, 'N').replace(/Ｏ/g, 'O').replace(/Ｐ/g, 'P').replace(/Ｑ/g, 'Q').replace(/Ｒ/g, 'R').replace(/Ｓ/g, 'S').replace(/Ｔ/g, 'T').replace(/Ｕ/g, 'U').replace(/Ｖ/g, 'V').replace(/Ｗ/g, 'W').replace(/Ｘ/g, 'X').replace(/Ｙ/g, 'Y').replace(/Ｚ/g, 'Z').replace(/ａ/g, 'a').replace(/ｂ/g, 'b').replace(/ｃ/g, 'c').replace(/ｄ/g, 'd').replace(/ｅ/g, 'e').replace(/ｆ/g, 'f').replace(/ｇ/g, 'g').replace(/ｈ/g, 'h').replace(/ｉ/g, 'i').replace(/ｊ/g, 'j').replace(/ｋ/g, 'k').replace(/ｌ/g, 'l').replace(/ｍ/g, 'm').replace(/ｎ/g, 'n').replace(/ｏ/g, 'o').replace(/ｐ/g, 'p').replace(/ｑ/g, 'q').replace(/ｒ/g, 'r').replace(/ｓ/g, 's').replace(/ｔ/g, 't').replace(/ｕ/g, 'u').replace(/ｖ/g, 'v').replace(/ｗ/g, 'w').replace(/ｘ/g, 'x').replace(/ｙ/g, 'y').replace(/ｚ/g, 'z');
         }
     }
+
+    /**
+     * Blogger用スタイル修正
+     */
+    const bloggerStyling = {
+        imageSizeToEnlarge: 720,
+        update: () => {
+            const $textarea = $('#blogger-article-html');
+            const originalArticle = $textarea.val();
+            // トップレベルに直接書かれたimgなどが不正なhtmlとして除去されるのを防ぐため、divに流し込む
+            let $article = $(`<div id="jquery-wrapper">${originalArticle}</div>`);
+
+            $article = bloggerStyling.enlargeImage($article);
+            $article = bloggerStyling.removeSpaceAroundImage($article);
+
+            $textarea.val($article.html());
+        },
+
+        enlargeImage: ($article) => {
+            const imageSizeToEnlarge = bloggerStyling.imageSizeToEnlarge;
+            $article.find('img').each((_, img) => {
+                const $img = $(img);
+                const src = $img.attr('src');
+                const dominantSize = $img.attr('width') === '640' ? 'width' : $img.attr('height') === '640' ? 'height' : null;
+
+                if (dominantSize) {
+                    $img.removeAttr(dominantSize === 'width' ? 'height' : 'width');
+                    $img.attr(dominantSize, imageSizeToEnlarge);
+                    const newSizeIndicator = dominantSize === 'width' ? '=w' : '=h';
+                    $img.attr('src', src.replace(/=w\d+$/, `${newSizeIndicator}${imageSizeToEnlarge}`));
+                }
+            });
+            return $article;
+        },
+
+        removeSpaceAroundImage: ($article) => {
+            $article.find('img').parent('a').each((_, anchor) => {
+                $(anchor).css({
+                    marginLeft: 0,
+                    marginRight: 0,
+                });
+            });
+            return $article;
+        },
+
+        bindEvents: () => {
+            $('#blogger-styling-button').bind('click', bloggerStyling.update);
+        },
+    };
+    bloggerStyling.bindEvents();
 
     /**
      * Base64エンコード・デコード
@@ -23,18 +70,16 @@ const main = () => {
             return decodeURIComponent(escape(atob(encoded)));
         },
         triggerEncode: () => {
-            const plain = $("#base64-plain").value;
-            const encoded = base64endec.encode(plain);
-            $("#base64-encoded").value = encoded;
+            const plain = _("#base64-plain").value;
+            _("#base64-encoded").value = base64endec.encode(plain);
         },
         triggerDecode: () => {
-            const encoded = $("#base64-encoded").value;
-            const plain = base64endec.decode(encoded);
-            $("#base64-plain").value = plain;
+            const encoded = _("#base64-encoded").value;
+            _("#base64-plain").value = base64endec.decode(encoded);
         },
         startWatching: () => {
-            $("#base64-plain").addEventListener("keyup", base64endec.triggerEncode);
-            $("#base64-encoded").addEventListener("keyup", base64endec.triggerDecode);
+            _("#base64-plain").addEventListener("keyup", base64endec.triggerEncode);
+            _("#base64-encoded").addEventListener("keyup", base64endec.triggerDecode);
         }
     };
     base64endec.startWatching();
@@ -43,10 +88,10 @@ const main = () => {
      * EasyTimer
      */
     const easyTimer = {
-        startStopButton: $("#et-toggle-button"),
-        combinedTimeInput: $("#et-combined"),
-        isAlertNeeded: $("#et-alert"),
-        result: $("#et-result"),
+        startStopButton: _("#et-toggle-button"),
+        combinedTimeInput: _("#et-combined"),
+        isAlertNeeded: _("#et-alert"),
+        result: _("#et-result"),
         intervalId: null,
         remainingSecondsInProgress: 0,
 
